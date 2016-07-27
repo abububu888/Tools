801dd8fa725a924fb04e3d1bbd46b9bce62a2072
abububu888 <abububu888@gmail.com>
2016-07-27 11:16:11 +0300

    Factory version 1

---
+++ Factory/FactoryPatternDemo.java
@@ -0,0 +1,25 @@
+
+public class FactoryPatternDemo {
+	  public static void main(String[] args) {
+	      ShapeFactory shapeFactory = new ShapeFactory();
+
+	      //get an object of Circle and call its draw method.
+	      Shape shape1 = shapeFactory.getShape(ShapeType.Circle);
+
+	      //call draw method of Circle
+	      shape1.draw();
+	      
+	      //get an object of Circle and call its draw method.
+	      Shape shape2 = shapeFactory.getShape(ShapeType.Rectangle);
+
+	      //call draw method of Circle
+	      shape2.draw();
+	      
+	      //get an object of Circle and call its draw method.
+	      Shape shape3 = shapeFactory.getShape(ShapeType.Square);
+
+	      //call draw method of Circle
+	      shape3.draw();
+
+	   }
+}
---
+++ Factory/ShapeType.java
@@ -0,0 +1,8 @@
+
+public enum ShapeType {
+
+	Rectangle,
+	Square,
+	Circle
+	
+}
